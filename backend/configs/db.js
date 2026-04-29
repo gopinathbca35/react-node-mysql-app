080c5cb6652ebcac98dd4984db26c77f75239d4c
gopinathbca35 <gopinathbca35@gmail.com>
2026-04-29 17:11:09 +0530

    Refactor DB connection to use environment variables

--- backend/configs/db.js
+++ backend/configs/db.js
@@ -1,17 +1,17 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-   host: 'mysql',
-   user: 'root',
-   password: 'root123',
-   database: 'booksdb'
+   host: process.env.DB_HOST || 'mysql',
+   user: process.env.DB_USER || 'root',
+   password: process.env.DB_PASSWORD || 'root123',
+   database: process.env.DB_NAME || 'booksdb'
 });
 
 function connectWithRetry() {
    db.connect((err) => {
       if (err) {
          console.log('⏳ Waiting for MySQL...');
-         setTimeout(connectWithRetry, 5000); // retry every 5 sec
+         setTimeout(connectWithRetry, 5000);
       } else {
          console.log('✅ Connected to MySQL');
       }
