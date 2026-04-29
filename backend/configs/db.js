41b5c795f660a618bef2fe8a7aac0a3cd6136120
gopinathbca35 <gopinathbca35@gmail.com>
2026-04-29 16:37:22 +0530

    Add retry mechanism for MySQL connection
    Implement connection retry logic for MySQL.

--- backend/configs/db.js
+++ backend/configs/db.js
@@ -2,18 +2,22 @@ const mysql = require('mysql2');
 
 const db = mysql.createConnection({
    host: 'mysql',
-   port: '3306',
    user: 'root',
    password: 'root123',
    database: 'booksdb'
 });
 
-db.connect((err) => {
-   if (err) {
-      console.error('Error connecting to MySQL:', err);
-   } else {
-      console.log('Connected to MySQL');
-   }
-});
+function connectWithRetry() {
+   db.connect((err) => {
+      if (err) {
+         console.log('⏳ Waiting for MySQL...');
+         setTimeout(connectWithRetry, 5000); // retry every 5 sec
+      } else {
+         console.log('✅ Connected to MySQL');
+      }
+   });
+}
+
+connectWithRetry();
 
 module.exports = db;
